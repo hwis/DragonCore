81b6faa7c3324cbd17d656f6534fef44e05e7adb
Hwis <freezymy1@mail.ru>
2024-09-10 14:58:05 +0300

    the war within start company (#5)
    * Create visions_of_atheroth.cpp
    
    * Create khazalgar_script_loader.cpp
    
    * Update khazalgar_script_loader.cpp
    
    * Update visions_of_atheroth.cpp
    
    * Update khazalgar_script_loader.cpp
    
    * Update spell_druid.cpp
    
    * Update spell_druid.cpp
    
    * Update visions_of_atheroth.cpp
    
    * Update spell_druid.cpp
    
    * Update visions_of_atheroth.cpp
    
    * Update spell_druid.cpp
    
    * Update spell_druid.cpp
    
    * Update visions_of_atheroth.cpp
    
    * change max player level and start evoker level in config file template
    
    * Update visions_of_atheroth.cpp
    
    * Update spell_druid.cpp
    
    * Update visions_of_atheroth.cpp

---
+++ src/server/scripts/KhazAlgar/khazalgar_script_loader.cpp
@@ -0,0 +1,7 @@
+void AddSC_visions_of_atheroth();
+// The name of this function should match:
+// void Add${NameOfDirectory}Scripts()
+void AddKhazAlgarScripts()
+{
+    AddSC_visions_of_atheroth();
+}
---
+++ src/server/scripts/KhazAlgar/visions_of_atheroth.cpp
@@ -0,0 +1,94 @@
+enum the_war_within
+{
+    QUEST_THE_WAR_WITHIN_A = 81930,
+    QUEST_THE_WAR_WITHIN_H = 78713
+};
+
+class add_start_quest_tww : public PlayerScript
+{
+public:
+    add_start_quest_tww() : PlayerScript("add_start_quest_tww") { }
+
+    void OnLevelChanged(Player* player, uint8 /*oldLevel*/)
+    {
+        Quest const* qA = sObjectMgr->GetQuestTemplate(QUEST_THE_WAR_WITHIN_A);
+        Quest const* qH = sObjectMgr->GetQuestTemplate(QUEST_THE_WAR_WITHIN_H);
+        if(player->GetQuestStatus(QUEST_THE_WAR_WITHIN_A) == QUEST_STATUS_REWARDED)
+            return;
+        if(player->GetQuestStatus(QUEST_THE_WAR_WITHIN_H) == QUEST_STATUS_REWARDED)
+            return;
+        if(player->GetLevel() >= 70)
+        {
+            if(player->GetTeam() == ALLIANCE)
+            {
+                if(player->GetQuestStatus(QUEST_THE_WAR_WITHIN_A) == QUEST_STATUS_NONE)
+                {
+                    player->AddQuestAndCheckCompletion(qA, player);
+                }
+            }
+            if(player->GetTeam() == HORDE)
+            {
+                if(player->GetQuestStatus(QUEST_THE_WAR_WITHIN_H) == QUEST_STATUS_NONE)
+                {
+                    player->AddQuestAndCheckCompletion(qH, player);
+                }
+            }
+        }
+    }
+};
+
+class quest_the_war_within : public QuestScript
+{
+public:
+    quest_the_war_within() : QuestScript("quest_the_war_within") { }
+
+    void OnQuestStatusChange(Player* player, Quest const* /*quest*/, QuestStatus /*oldStatus*/, QuestStatus newStatus)
+    {
+        Quest const* qA = sObjectMgr->GetQuestTemplate(QUEST_THE_WAR_WITHIN_A);
+        Quest const* qH = sObjectMgr->GetQuestTemplate(QUEST_THE_WAR_WITHIN_H);
+        if(player->GetQuestStatus(QUEST_THE_WAR_WITHIN_A) == QUEST_STATUS_REWARDED)
+            return;
+        if(player->GetQuestStatus(QUEST_THE_WAR_WITHIN_H) == QUEST_STATUS_REWARDED)
+            return;
+        if(player->GetLevel() >= 70)
+        {
+            if(player->GetTeam() == ALLIANCE)
+            {
+                if(newStatus == QUEST_STATUS_NONE)
+                {
+                    player->AddQuestAndCheckCompletion(qA, player);
+                }
+            }
+            if(player->GetTeam() == HORDE)
+            {
+                if(newStatus == QUEST_STATUS_NONE)
+                {
+                    player->AddQuestAndCheckCompletion(qH, player);
+                }
+            }
+        }
+    }
+};
+
+// spell 227669 tele to silithus
+class spell_tele_to_silithus : public SpellScript
+{
+public:
+    void HandleDummy(SpellEffIndex /*effindex*/)
+    {
+        Player* player = GetCaster()->ToPlayer();
+        player->TeleportTo(1, -7070, 1269, -90, 2.39f);
+    }
+
+    void Register() override
+    {
+        OnEffectHitTarget += SpellEffectFn(spell_tele_to_silithus::HandleDummy, EFFECT_0, SPELL_EFFECT_DUMMY);
+    }
+};
+
+void AddSC_visions_of_atheroth()
+{
+    new add_start_quest_tww();
+    new quest_the_war_within();
+    RegisterSpellScript(spell_tele_to_silithus);
+}
