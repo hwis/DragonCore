237335a549d9ed3818ca42513e4990e5a79e584d
hwis <freezymy1@mail.ru>
2025-02-23 18:19:03 +0000

    Advanced flying

---
+++ sql/updates/world/master/2025_02_23_10_world.sql
@@ -0,0 +1,18 @@
+DELETE FROM spell_script_names WHERE ScriptName IN (
+"spell_af_skyriding",
+"spell_af_skyward_ascent",
+"spell_af_energy",
+"spell_af_surge_forward",
+"spell_af_whirling_surge",
+"spell_switch_flight");
+
+INSERT INTO spell_script_names VALUES
+(373646, "spell_af_skyriding"),
+(406095, "spell_af_skyriding"),
+(430833, "spell_af_skyriding"),
+(374763, "spell_af_skyward_ascent"),
+(372610, "spell_af_skyward_ascent"),
+(372773, "spell_af_energy"),
+(372608, "spell_af_surge_forward"),
+(361584, "spell_af_whirling_surge"),
+(436854, "spell_switch_flight");
