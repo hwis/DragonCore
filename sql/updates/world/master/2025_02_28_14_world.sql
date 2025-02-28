b9b36f4e025c9891b85f344f6e41535abd8e6bb0
hwis <freezymy1@mail.ru>
2025-02-28 18:52:13 +0000

    DB/Spells: [ Druid ] [ Matted Fur ] [ Proc Data]

---
+++ sql/updates/world/master/2025_02_28_14_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc` WHERE `SpellId`=385786;
+INSERT INTO `spell_proc` (`SpellId`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `SpellFamilyMask3`, `ProcFlags`, `ProcFlags2`, `SpellTypeMask`, `SpellPhaseMask`, `HitMask`, `AttributesMask`, `DisableEffectsMask`, `ProcsPerMinute`, `Chance`, `Cooldown`, `Charges`) VALUES 
+(385786,0,7,0x00000000,0x00040000,0x00000000,0x00000000,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0,0,0,0); -- Matted Fur
