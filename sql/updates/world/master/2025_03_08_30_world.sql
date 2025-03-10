ad15afd21510d23e5316bfbaad04555c1d83258d
hwis <hwisrwx@gmail.com>
2025-03-10 12:56:09 +0000

    DB/Spells: Templar Strikes proc

---
+++ sql/updates/world/master/2025_03_08_30_world.sql
@@ -0,0 +1,8 @@
+DELETE FROM `spell_proc` WHERE `SpellId`=406646;
+INSERT INTO `spell_proc` (`SpellId`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`SpellFamilyMask3`,`ProcFlags`,`ProcFlags2`,`SpellTypeMask`,`SpellPhaseMask`,`HitMask`,`AttributesMask`,`DisableEffectsMask`,`ProcsPerMinute`,`Chance`,`Cooldown`,`Charges`) VALUES
+(406646,0x00,10,0x08000000,0x00000000,0x00000042,0x00080000,0x0,0x0,0x1,0x2,0x0,0x0,0x0,0,0,0,0); -- Templar Strikes
+
+DELETE FROM `spell_proc` WHERE `SpellId`=406648;
+-- INSERT INTO `spell_proc` (`SpellId`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`SpellFamilyMask3`,`ProcFlags`,`ProcFlags2`,`SpellTypeMask`,`SpellPhaseMask`,`HitMask`,`AttributesMask`,`DisableEffectsMask`,`ProcsPerMinute`,`Chance`,`Cooldown`,`Charges`) VALUES
+-- (406648,0x00,10,0x08000000,0x00000000,0x00000042,0x00080000,0x0,0x0,0x1,0x2,0x0,0x0,0x0,0,0,0,0); -- Templar Strikes
+
