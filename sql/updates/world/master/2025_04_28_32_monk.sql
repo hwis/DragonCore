684ad683c00918b7257da94270bdf15e0ea53458
hwis <hwisrwx@gmail.com>
2025-04-28 18:26:52 +0000

    Scripts/Spell: Monks spell Vivify fix

---
+++ sql/updates/world/master/2025_04_28_32_monk.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_script_names` WHERE `spell_id`=116670 AND `ScriptName`='spell_monk_vivify';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(116670, 'spell_monk_vivify');
