10811dff715a563b26228c586fe1652f6ad4e88c
hwis <hwisrwx@gmail.com>
2025-06-11 09:46:25 +0000

    Scripts/Spell: fix Evoker talent Eternity Surge

---
+++ sql/updates/world/master/2025_06_11_30_world_evo_eternity_surge.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_script_names` WHERE (`spell_id`=359073 AND `ScriptName`='spell_evo_eternity_surge');
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(359073, 'spell_evo_eternity_surge');
