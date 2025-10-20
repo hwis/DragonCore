8a8834fa24ff53c028f96feaaccf0ce63395944d
hwis <hwisrwx@gmail.com>
2025-10-20 11:43:58 +0000

    followup adv flying

--- src/server/scripts/World/adv_flying_check.cpp
+++ src/server/scripts/World/adv_flying_check.cpp
@@ -7,6 +7,22 @@ class adv_flying_check : public PlayerScript
 public:
     adv_flying_check() : PlayerScript("adv_flying_check") { }
 
+    void OnLogin(Player* player, bool /*firstLogin*/) override
+    {
+        if (player->GetLevel() >= 10)
+        {
+            if (!player->HasAura(404468) || !player->HasAura(464464))
+                player->AddAura(404464, player);
+
+            if (!player->HasSpell(376777))
+                player->LearnSpell(376777, false);
+        }
+
+        if (player->GetLevel() >= 20)
+            if (!player->HasSpell(436854))
+                player->LearnSpell(436854, false);
+    }
+
     void OnLevelChanged(Player* player, uint8 oldLevel) override
     {
         if (player->GetLevel() >= 10)
