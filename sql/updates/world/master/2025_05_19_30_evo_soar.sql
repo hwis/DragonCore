6e8f2ecc7fed0cc1fc8e30a4e415129e89a69d47
hwis <hwisrwx@gmail.com>
2025-10-24 10:01:50 +0000

    Script/Spells: fix Evoker spell Soar

---
+++ sql/updates/world/master/2025_05_19_30_evo_soar.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_script_names` WHERE (`spell_id`=369536 AND `ScriptName`='spell_evo_soar') OR (`spell_id`=430747 AND `ScriptName`='spell_evo_soar_aura');
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(369536, 'spell_evo_soar'),
+(430747, 'spell_evo_soar_aura');
