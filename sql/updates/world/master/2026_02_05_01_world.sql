4cd25624d58f6fb49d257d79129c5a144990cc3a
hwis <hwisrwx@gmail.com>
2026-02-19 05:28:06 +0000

    Scripts/Spells: Druids talent Maim fix

---
+++ sql/updates/world/master/2026_02_05_01_world.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_script_names` WHERE `spell_id`=22570 AND `ScriptName`='spell_dru_maim';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(22570, 'spell_dru_maim');
